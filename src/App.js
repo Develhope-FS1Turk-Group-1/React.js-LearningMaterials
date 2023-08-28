b42d58206f412f5698d5b1e32dd497a8e5614c35
Okkes <okkesakkurt46@gmail.com>
2023-08-28 19:32:55 +0300

    Introduction to React.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{ Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0,
+        };
+    }
+
+    incrementCount =  () => {
+         this.setState((prevState) => ({
+            count: prevState.count + 1,
+        }));
+    };
+    /*
+    incrementCount = () => {
+         this.setState({
+             ['count']: this.state.count + 1,
+         });
+
+    };
+*/
+    render() {
+        const {count} = this.state;
+        const name = 'Okkes';
+
+        return (
+            <div className='App'>
+                <header className='App-header'>
+                    <img
+                        src={logo}
+                        className='App-logo'
+                        alt='logo'
+                    />
+              {name}
+              <br />
+              {count}
+              <br />
+                    <button
+                        onClick={() => {
+                            this.incrementCount();
+                            console.log(count);
+                        }}>
+                        CLick me
+                    </button>
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className='App-link'
+                        href='https://reactjs.org'
+                        target='_blank'
+                        rel='noopener noreferrer'>
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
